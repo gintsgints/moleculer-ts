f93ae941993be16c53ba7b4732ffc6cca63908b4
Gints Polis <gints.polis@lattelecom.lv>
2019-07-14 22:18:41 +0300

    My corrections.

--- services/greeter.service.ts
+++ services/greeter.service.ts
@@ -1,84 +1,84 @@
-"use strict";
-import { ServiceSchema } from "moleculer";
+'use strict'
+import { ServiceSchema } from 'moleculer'
 
 const GreeterService: ServiceSchema = {
-	name: "greeter",
+  name: 'greeter',
 
-	/**
-	 * Service settings
-	 */
-	settings: {
+  /**
+   * Service settings
+   */
+  settings: {
 
-	},
+  },
 
-	/**
-	 * Service dependencies
-	 */
-	dependencies: [],
+  /**
+   * Service dependencies
+   */
+  dependencies: [],
 
-	/**
-	 * Actions
-	 */
-	actions: {
+  /**
+   * Actions
+   */
+  actions: {
 
-		/**
-		 * Say a 'Hello'
-		 *
-		 * @returns
-		 */
-		hello() {
-			return "Hello Moleculer";
-		},
+    /**
+     * Say a 'Hello'
+     *
+     * @returns
+     */
+    hello() {
+      return 'Hello Moleculer'
+    },
 
-		/**
-		 * Welcome a username
-		 *
-		 * @param {String} name - User name
-		 */
-		welcome: {
-			params: {
-				name: "string",
-			},
-			handler(ctx) {
-				return `Welcome, ${ctx.params.name}`;
-			},
-		},
-	},
+    /**
+     * Welcome a username
+     *
+     * @param {String} name - User name
+     */
+    welcome: {
+      params: {
+        name: 'string',
+      },
+      handler(ctx) {
+        return `Welcome, ${ctx.params.name}`
+      },
+    },
+  },
 
-	/**
-	 * Events
-	 */
-	events: {
+  /**
+   * Events
+   */
+  events: {
 
-	},
+  },
 
-	/**
-	 * Methods
-	 */
-	methods: {
+  /**
+   * Methods
+   */
+  methods: {
 
-	},
+  },
 
-	/**
-	 * Service created lifecycle event handler
-	 */
-	created() {
+  /**
+   * Service created lifecycle event handler
+   */
+  created() {
+    console.log('created')
+  },
 
-	},
+  /**
+   * Service started lifecycle event handler
+   */
+  // started() {
 
-	/**
-	 * Service started lifecycle event handler
-	 */
-	// started() {
+  // },
 
-	// },
+  /**
+   * Service stopped lifecycle event handler
+   */
+  // stopped() {
 
-	/**
-	 * Service stopped lifecycle event handler
-	 */
-	// stopped() {
+  // },
+}
 
-	// },
-};
-
-export = GreeterService;
+export = GreeterService
